39ca43348656b416476bff41afc50721b23c6335
christopher.moses <christopher.moses@ui.ac.id>
2023-03-20 16:57:45 +0700

    debugging only

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,32 @@
-import { useEffect, useState } from "react";
-import Logout from "../components/logout";
+
 import checkRole from "@/components/Helper/CheckRole";
-// import { getCookie, useCookie } from "@/components/Helper/cookies";
-import { getCookie } from "@/components/Helper/cookies";
-import { useCookie } from "@/components/Hooks/useCookie";
-import { useRouter } from "next/router";
-import axios from "axios";
-import Profile from '../components/Profile';
-import Button from "@/components/Button";
+
 
 export default function index(props) {
-  const router = useRouter();
-  
-  const setAllProfile = () => {
-    
-    router.push("/profile/listuser");
-  };
-  const gantiPassword = () => {
-    router.push("/profile/ChangePassword")
-  }
-  const registerGuru = () => {
-    router.push("/registrasi/Guru")
-  }
-  const registerSiswa = () => {
-    router.push("/registrasi/Siswa")
-  }
-
-  if(props.role==="ADMIN"){
-
-    return (
-      <div className="ml-auto mr-auto px-56">
-        <img width={1400} height={1400} src="assets/PASTrack.svg"></img>
-      </div>
-    );
-  }
+  const API_ROUTE = process.env.NEXT_PUBLIC_API_ROUTE;
+  console.log(API_ROUTE);
+  return (
+    <div className="ml-auto mr-auto px-56">
+      <img width={1400} height={1400} src="/assets/PASTrack.svg"></img>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
   // context.req.query
-  const authentications = checkRole(context)
-  if(!authentications.tokenTrue) {
+  const authentications = checkRole(context);
+  if (!authentications.tokenTrue) {
     return {
       redirect: {
         destination: "/login",
         permanent: false,
-      }
-    }
+      },
+    };
   }
   const { role } = context.req.cookies;
   return {
     props: {
-      role: role
-    }
-  }
+      role: role,
+    },
+  };
 }
