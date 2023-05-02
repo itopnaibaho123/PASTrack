9cb7ce46caed1e90092db03df5e6ef765df5c43c
Alviona Retno Amalia <alviona.retno@ui.ac.id>
2023-05-02 10:23:25 +0700

    deleted logo pastrack

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,7 +14,6 @@ export default function login() {
       <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img className="mx-auto h-28 w-auto" src="/assets/PASTrack.svg" alt="PASTrack Logo" />
-          <H2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome to PASTrack</H2>
         </div>
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
