39639d88ec6dab844764e821d7759e63c1fe2e8b
Alviona Retno Amalia <alviona.retno@ui.ac.id>
2023-05-22 12:36:50 +0700

    click on matpel siswa

--- src/components/CardMatpelKelas.js
+++ src/components/CardMatpelKelas.js
@@ -1,28 +1,24 @@
 import { useRouter } from "next/router";
 import React from "react";
 
-export default function CardMatpelKelas({ matpel, role, username }) {
+export default function CardMatpelKelas({ matpel, username, role }) {
   const router = useRouter();
   if (role === "MURID") {
-    matpel.map((s) => {
-      return (
-        <div
-          className="bg-white text-emerald-500 border border-blue-500 px-4 py-2 rounded-lg"
-          onClick={() =>
-            router.push(`${router.push(`pelajaran/${s.id}/siswa/${username}`)}`)
-          }
-        >
-          {s.namaMataPelajaran}
-        </div>
-      );
-    });
+    console.log(matpel.id)
+    return (
+      <div
+        className="bg-white text-emerald-500 border border-blue-500 px-4 py-2 rounded-lg"
+        onClick={() =>
+          router.push(`/pelajaran/${matpel.id}/siswa/${username}`)}
+      >
+        {matpel.namaMataPelajaran}
+      </div>
+    );
   } else {
-    matpel.map((s) => {
-      return (
-        <div className="bg-white text-emerald-500 border border-blue-500 px-4 py-2 rounded-lg">
-          {s.namaMataPelajaran}
-        </div>
-      );
-    });
+    return (
+      <div className="bg-white text-emerald-500 border border-blue-500 px-4 py-2 rounded-lg">
+        {matpel.namaMataPelajaran}
+      </div>
+    );
   }
 }
